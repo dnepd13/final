29a2244068cdf87382b7cc1a0fea3473d6796940
user1 <user1@DESKTOP-0IFG55Q>
2020-02-14 18:31:25 +0900

    판매자아이디 중복확인 기능

--- final_project/src/main/java/com/kh/ordering/repository/SellerDao.java
+++ final_project/src/main/java/com/kh/ordering/repository/SellerDao.java
@@ -2,6 +2,8 @@ package com.kh.ordering.repository;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.ordering.entity.CustomOrderDto;
@@ -13,10 +15,21 @@ import com.kh.ordering.vo.CustomOrderVO;
 public interface SellerDao {
 	int getSequence();
 	void regist(SellerDto sellerDto);
-	
+	//////판매자 로그인//////
 	SellerDto login(SellerDto sellerDto);
+	/////판매자 정보조회////
 	SellerDto info(SellerDto sellerDto);
-//	SellerDto info_edit(SellerDto sellerDto);
+	//판매자 정보 수정//////
+	SellerDto info_edit(SellerDto sellerDto);
+	
+	//판매자 회원탈퇴
+	void delete(SellerDto sellerDto);
+	void changepw(SellerDto sellerDto);
+	
+
+		int id_check(String seller_id);
+
+
 
 
 
