dca29896da17da02ae050b5895bebc415a55e186
진 <@DESKTOP-IE13CT6>
2020-02-26 13:44:34 +0900

    주문제작 사진

--- final_project_admin/src/main/java/com/kh/admin/repository/GoodsFilesDao.java
+++ final_project_admin/src/main/java/com/kh/admin/repository/GoodsFilesDao.java
@@ -8,4 +8,7 @@ public interface GoodsFilesDao {
 	public List<FilesDto> getGoodsFiles(int goods_no);
 	public FilesDto getFile(int files_no);
 	
+	
+	//주문제작 사진 가져오기
+	public List<FilesDto> getCustomOrderFiles(int custom_files_no);
 }
