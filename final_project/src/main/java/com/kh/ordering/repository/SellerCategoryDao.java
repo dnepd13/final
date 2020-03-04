25c6464aed5c6d5d2a032f1f58674746baf1c045
yunseong-gyeong <yunseong-gyeong@218.38.137.27>
2020-03-04 23:00:34 +0900

    포트폴리오 구현중

--- final_project/src/main/java/com/kh/ordering/repository/SellerCategoryDao.java
+++ final_project/src/main/java/com/kh/ordering/repository/SellerCategoryDao.java
@@ -2,11 +2,16 @@ package com.kh.ordering.repository;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.kh.ordering.entity.CategoryDto;
 import com.kh.ordering.entity.SellerCategoryDto;
 
 public interface SellerCategoryDao {
 	public int categoryCount();
+	//판매자 아이디로 번호 가져오기
+		int getNo(@RequestParam String seller_id);
+		
 	void seller_category_insert(SellerCategoryDto sellerCategoryDto);
 
 	void seller_update_category(SellerCategoryDto sellerCategoryDto);
